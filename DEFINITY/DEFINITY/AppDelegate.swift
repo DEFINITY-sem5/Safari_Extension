55cdfff679ef65cb0fb8bb6e50d2a27a6a599e59
ashok-mbp16 <ashok_a380@icloud.com>
2020-10-23 18:42:08 +0530

    safari plugin initialise

---
+++ DEFINITY/DEFINITY/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  DEFINITY
+//
+//  Created by REGATTE on 23/10/20.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+}
